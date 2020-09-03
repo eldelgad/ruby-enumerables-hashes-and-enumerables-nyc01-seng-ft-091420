71624fc39cdbc555de274934a4e4688cd0ac429e
Elias Delgado <evdelgado99@gmail.com>
2020-09-03 04:55:44 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -20,11 +20,11 @@ def select_winner(passengers)
   
 end
 
-birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
+# birthday_kids = {
+#   "Timmy" => 9,
+#   "Sarah" => 6,
+#   "Amanda" => 27
+# }
 
 def happy_birthday(birthday_kids)
   
